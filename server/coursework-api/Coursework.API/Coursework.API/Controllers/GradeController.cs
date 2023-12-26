9a068e2001650b16b9a9ae2922c5110b284ef421
Sebastian <sebastianprogramming99@gmail.com>
2023-12-25 20:09:25 -0500

    Using IActionResult in controller, updating controller for test case

--- server/coursework-api/Coursework.API/Coursework.API/Controllers/GradeController.cs
+++ server/coursework-api/Coursework.API/Coursework.API/Controllers/GradeController.cs
@@ -1,6 +1,4 @@
 using Coursework.Core.Models;
-using Coursework.Core.Repositories;
-using Coursework.Core.Services;
 using Microsoft.AspNetCore.Mvc;
 using Coursework.Infrastructure.SQL.Models;
 using Microsoft.EntityFrameworkCore;
@@ -12,21 +10,23 @@ namespace Coursework.API.Controllers;
 public class GradeController : ControllerBase
 {
     private readonly ILogger<GradeController> _logger;
-    private readonly GradesService gradesService;
     private readonly CourseworkDBContext _courseworkDbContext;
 
-    public GradeController(ILogger<GradeController> logger, CourseworkDBContext _courseworkDbContext)
+    public GradeController(ILogger<GradeController> logger, CourseworkDBContext dbContext)
     {
         _logger = logger;
-        gradesService = new GradesService(new GradesRepository(_courseworkDbContext));
+        _courseworkDbContext = dbContext;
     }
     
     // Get Method to support grabbing a grade by ID {id:int} is a template so it takes an int
     // called id and the "id" is part of that route's name
     [HttpGet("{id:int}")]
-    public Grade Get(
+    public IActionResult Get(
         [FromRoute] int id)
     {
-        return gradesService.FindOne(id);
+        var gradesTable = _courseworkDbContext.Grade;
+        var grade = gradesTable.FirstOrDefault(grade => grade.Id == id);
+
+        return Ok(grade ?? new Grade() { Id = 0, grade = "OOPS" });
     }
 }
